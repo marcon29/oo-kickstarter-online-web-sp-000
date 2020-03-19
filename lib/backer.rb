e3e4e7c10ba51763e3b785289ae4180288253970
Mark Stabler <mark@markstabler.com>
2020-03-19 02:29:22 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,21 +1,21 @@
 class Backer
   attr_accessor :name, :backed_projects
 
-  @@all = []
+  #@@all = []
 
   def initialize(name)
     @name = name
     @backed_projects = []
-    save
+  #  save
   end
 
-  def save
-    @@all << self
-  end
+  #def save
+  #  @@all << self
+  #end
 
-  def self.all
-    @@all
-  end
+  #def self.all
+  #  @@all
+  #end
 
   def back_project(project)
     @backed_projects << project
