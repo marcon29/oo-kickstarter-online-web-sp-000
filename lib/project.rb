440fd8556c0d83b8b500fe3dcdb91e50697027e4
Mark Stabler <mark@markstabler.com>
2020-03-19 02:41:30 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,21 +1,10 @@
 class Project
   attr_accessor :title, :backers
 
-  #@@all = []
-
   def initialize(title)
     @title = title
     @backers = []
-  #  save
   end
-
-  #def save
-  #  @@all << self
-  #end
-
-  #def self.all
-  #  @@all
-  #end
 
   def add_backer(backer)
     @backers << backer
